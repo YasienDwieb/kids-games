b0f04d6f7169f0a4d2f1de26d17c732c8ce4a482
Yasien Dwieb <yasien.dwieb@gmail.com>
2026-02-06 21:02:43 +0200

    delete example game

--- src/games/index.ts
+++ src/games/index.ts
@@ -1,5 +1,4 @@
 export { registerGame, getGame, getAllGames, getGamesForAge } from './registry';
 
 // Import game configs here to auto-register them
-// import './example/config';
 import './simple-pairs/config';
